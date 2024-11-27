c7588edb01ddcc37feddf5b30a462c002e57a4b4
rhithesh <rhithesh1947@gmail.com>
2024-11-27 13:55:11 +0530

    change

--- components/PriceFeed/ProtfolioComponent.tsx
+++ components/PriceFeed/ProtfolioComponent.tsx
@@ -5,8 +5,8 @@ import React from 'react'
 function ProtfolioComponent() {
   return (
 
-    <div className=' flex justify-center items-center ml-[430px] bg-black  w-[20em] h-[30em]'>
-      <p className=' text-white'>Protfolio Comming Soon!
+    <div className=' flex justify-center rounded-xl items-center ml-[430px] bg-white  w-[20em] h-[30em]'>
+      <p className=' text-black'>Protfolio Comming Soon!
 
       </p>
       </div>
